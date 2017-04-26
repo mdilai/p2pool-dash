p2pool/p2pool

Minor tweaks to logging and to stratum behavior upon restarts

p2pool/work.py
@@ -349,7 +349,7 @@ class WorkerBridge(worker_interface.WorkerBridge):
                 # 1/100th the difficulty of a full share should be a reasonable upper bound. That way, if
                 # one node has the whole p2pool hashrate, it will still only need to process one pseudoshare
                 # every 0.3 seconds.
-                target = min(target, 100 * bitcoin_data.average_attempts_to_target((bitcoin_data.target_to_average_attempts(
+                target = min(target, 1000 * bitcoin_data.average_attempts_to_target((bitcoin_data.target_to_average_attempts(
                     self.node.bitcoind_work.value['bits'].target)*self.node.net.SPREAD)*self.node.net.PARENT.DUST_THRESHOLD/block_subsidy))
         else:
             target = desired_pseudoshare_target

mdilai/p2pool-dash

p2pool/work.py
@@ -368,7 +368,7 @@ class WorkerBridge(worker_interface.WorkerBridge):
                 # 1/100th the difficulty of a full share should be a reasonable upper bound. That way, if
                 # one node has the whole p2pool hashrate, it will still only need to process one pseudoshare
                 # every 0.3 seconds.
-                target = min(target, 100 * dash_data.average_attempts_to_target((dash_data.target_to_average_attempts(
+                target = min(target, 1000 * dash_data.average_attempts_to_target((dash_data.target_to_average_attempts(
                     self.node.dashd_work.value['bits'].target)*self.node.net.SPREAD)*self.node.net.PARENT.DUST_THRESHOLD/block_subsidy))
         else:
             target = desired_pseudoshare_target