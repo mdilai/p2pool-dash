p2pool/p2pool

Fix "tried to broadcast share without knowing all transactions" issue: Make sure we don't forget the tx that are currently being mined (and not just the ones that are part of the most recently received block template), and add a few failsafes in case that happens. https://bitcointalk.org/index.php?topic=18313.msg18597829#msg18597829

p2pool/node.py
@@ -17,7 +17,8 @@ class P2PNode(p2p.Node):
             best_share_hash_func=lambda: node.best_share_var.value,
             net=node.net,
             known_txs_var=node.known_txs_var,
-            mining_txs_var=node.mining_txs_var,
+            mining_txs_var=node.mining_txs_var,   # transactions from getblocktemplate
+            mining2_txs_var=node.mining2_txs_var, # transactions sent to miners
         **kwargs)
     
     def handle_shares(self, shares, peer):
@@ -221,6 +222,7 @@ class Node(object):
         
         self.known_txs_var = variable.VariableDict({}) # hash -> tx
         self.mining_txs_var = variable.Variable({}) # hash -> tx
+        self.mining2_txs_var = variable.Variable({}) # hash -> tx
         self.get_height_rel_highest = yield height_tracker.get_height_rel_highest_func(self.bitcoind, self.factory, lambda: self.bitcoind_work.value['previous_block'], self.net)
         
         self.best_share_var = variable.Variable(None)
@@ -233,11 +235,8 @@ class Node(object):
         # update mining_txs according to getwork results
         @self.bitcoind_work.changed.run_and_watch
         def _(_=None):
-            new_mining_txs = {}
-            added_known_txs = {}
-            for tx_hash, tx in zip(self.bitcoind_work.value['transaction_hashes'], self.bitcoind_work.value['transactions']):
-                new_mining_txs[tx_hash] = tx
-                added_known_txs[tx_hash] = tx
+            new_mining_txs = dict(zip(self.bitcoind_work.value['transaction_hashes'], self.bitcoind_work.value['transactions']))
+            added_known_txs = {hsh:tx for hsh,tx in new_mining_txs.iteritems() if not hsh in self.known_txs_var.value}
             self.mining_txs_var.set(new_mining_txs)
             self.known_txs_var.add(added_known_txs)
         # add p2p transactions from bitcoind to known_txs
@@ -276,6 +275,7 @@ class Node(object):
                 for peer in self.p2p_node.peers.itervalues():
                     new_known_txs.update(peer.remembered_txs)
             new_known_txs.update(self.mining_txs_var.value)
+            new_known_txs.update(self.mining2_txs_var.value)
             for share in self.tracker.get_chain(self.best_share_var.value, min(120, self.tracker.get_height(self.best_share_var.value))):
                 for tx_hash in share.new_transaction_hashes:
                     if tx_hash in self.known_txs_var.value:

mdilai/p2pool-dash

p2pool/node.py
@@ -17,7 +17,8 @@ class P2PNode(p2p.Node):
             best_share_hash_func=lambda: node.best_share_var.value,
             net=node.net,
             known_txs_var=node.known_txs_var,
-            mining_txs_var=node.mining_txs_var,
+            mining_txs_var=node.mining_txs_var,   # transactions from getblocktemplate
+            mining2_txs_var=node.mining2_txs_var, # transactions sent to miners
         **kwargs)
     
     def handle_shares(self, shares, peer):
@@ -221,6 +222,7 @@ class Node(object):
         
         self.known_txs_var = variable.VariableDict({}) # hash -> tx
         self.mining_txs_var = variable.Variable({}) # hash -> tx
+        self.mining2_txs_var = variable.Variable({}) # hash -> tx
         self.get_height_rel_highest = yield height_tracker.get_height_rel_highest_func(self.dashd, self.factory, lambda: self.dashd_work.value['previous_block'], self.net)
         
         self.best_share_var = variable.Variable(None)
@@ -233,11 +235,8 @@ class Node(object):
         # update mining_txs according to getwork results
         @self.dashd_work.changed.run_and_watch
         def _(_=None):
-            new_mining_txs = {}
-            added_known_txs = {}
-            for tx_hash, tx in zip(self.dashd_work.value['transaction_hashes'], self.dashd_work.value['transactions']):
-                new_mining_txs[tx_hash] = tx
-                added_known_txs[tx_hash] = tx
+            new_mining_txs = dict(zip(self.dashd_work.value['transaction_hashes'], self.dashd_work.value['transactions']))
+            added_known_txs = {hsh:tx for hsh,tx in new_mining_txs.iteritems() if not hsh in self.known_txs_var.value}
             self.mining_txs_var.set(new_mining_txs)
             self.known_txs_var.add(added_known_txs)
         # add p2p transactions from dashd to known_txs
@@ -277,6 +276,7 @@ class Node(object):
                 for peer in self.p2p_node.peers.itervalues():
                     new_known_txs.update(peer.remembered_txs)
             new_known_txs.update(self.mining_txs_var.value)
+            new_known_txs.update(self.mining2_txs_var.value)
             for share in self.tracker.get_chain(self.best_share_var.value, min(120, self.tracker.get_height(self.best_share_var.value))):
                 for tx_hash in share.new_transaction_hashes:
                     if tx_hash in self.known_txs_var.value: